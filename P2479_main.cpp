196075a218f89e0fdd7ab8baf7e726782a98a7fc
New Demo <New Demo>
2016-09-08 20:05:01 +0800

    Need to optimize the time cost.

--- P2479_main.cpp
+++ P2479_main.cpp
@@ -2,25 +2,80 @@
 #include<vector>
 #include<algorithm>
 
+
+int findMaxSum_sequence(std::vector<int> sequence, int end)
+{
+	int maxSum = 0;
+	int currentSum = 0;
+	for (int i = 0; i < end; ++i)
+	{
+		currentSum += sequence[i];
+		if (currentSum > maxSum)
+		{
+			maxSum = currentSum;
+		}
+		else
+		{
+			if (currentSum < 0)
+			{
+				currentSum = 0;
+			}
+		}
+	}
+	return maxSum;
+}
+
+int findMaxSum_reverse(std::vector<int> sequence, int end)
+{
+	int maxSum = 0;
+	int currentSum = 0;
+	for (int i = sequence.size() - 1; i >= end; --i)
+	{
+		currentSum += sequence[i];
+		if (currentSum > maxSum)
+		{
+			maxSum = currentSum;
+		}
+		else
+		{
+			if (currentSum < 0)
+			{
+				currentSum = 0;
+			}
+		}
+	}
+	return maxSum;
+}
+
 int main()
 {
 	int T = 0;
-	while (std::cin >> T)
+	while (scanf_s("%d", &T) != EOF)
 	{
 		for (int i = 0; i < T; ++i)
 		{
 			int n = 0;
-			std::cin >> n;
+			scanf_s("%d",&n);
 			std::vector<int> sequence(n);
 			for (int j = 0; j < n; ++j)
 			{
-				std::cin >> sequence.at(j);
+				scanf_s("%d",&sequence.at(j));
 			}
-			for (int j = 0; j < n; ++j)
+			/*for (int j = 0; j < n; ++j)
 			{
 				std::cout << sequence.at(j) << " ";
 			}
-			std::cout << std::endl;
+			std::cout << std::endl;*/
+			int maxSum = 0;
+			for (int j = 0; j < n; ++j)
+			{
+				int currentSum = findMaxSum_sequence(sequence, j) + findMaxSum_reverse(sequence, j + 1);
+				if (currentSum > maxSum)
+				{
+					maxSum = currentSum;
+				}
+			}
+			printf("%d\n", maxSum);
 		}
 	}
 	return 0;
